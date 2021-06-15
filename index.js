aa2ffe8bd04b0117885b673445ed7e401a555617
danmada <danmada@gmail.com>
2021-06-15 09:48:58 -0500

    commit tree 0ed0c6f3a93f7f99a2287bc6dba4d22e646c3b26

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function() {
+    const greeting = document.getElementById("text")
+    greeting.textContent = "This is really cool!"
+})
+
